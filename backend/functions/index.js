2b702a897cbfc3cdf252d0344743501797b23f96
Kevin <beaver2025@gmail.com>
2020-04-26 11:01:54 -0400

    Capacitor

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -5,23 +5,33 @@ const { admin, db } = require('./util/admin');
 
 const { signup, login, uploadImage } = require('./handlers/stores')
 
+//FB (FireBase) authentication middleware
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('No token found');
+        return res.status(403).json({ error: 'Unauthorized '});
+    }
+
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            req.user = decodedToken;
+            console.log('Decoded token', decodedToken);
+            return db.collection('stores')
+                .where('userId', '==', req.user.uid)
+                .limit(1)
+                .get()
+        })
+        .then(data => {
+            req.user.handle = data.docs[0].data().handle;
+        })
+}
+
 // Store routes
 app.post('/signup', signup);
 app.post('/login', login);
-app.post('/user/image', uploadImage);
-
-// exports.getStores = functions.https.onRequest((req, res) => {
-//     db.collection('stores').get()
-//         .then(data => {
-//             let stores = [];
-
-//             data.forEach(doc => {
-//                 stores.push(doc.data())
-//             })
-
-//             return res.json(stores);
-//         })
-//         .catch(err => console.error(err));
-// });
+app.post('/user/image', FBAuth, uploadImage);
 
 exports.api = functions.region('us-east4').https.onRequest(app);
