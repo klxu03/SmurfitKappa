bc4147eb786eabc9b83f983c817e6dca35402bdb
Kevin <beaver2025@gmail.com>
2020-04-26 18:25:19 -0400

    Front end done

--- enterprise/src/App.js
+++ enterprise/src/App.js
@@ -31,6 +31,7 @@ import './theme/variables.css';
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [userToken, setUserToken] = useState();
 
   useEffect(() => {
     setLoggedIn(false);
@@ -41,9 +42,16 @@ const App = () => {
       <IonReactRouter>
         <IonTabs>
           <IonRouterOutlet>
-            <Route path="/profile" component={loggedIn ? Profile : Login} />
-            <Route path="/home" component={Home} exact />
-            <Redirect exact from="/" to="/home" />
+            {/* <Route path="/profile" component={loggedIn ? Profile : Login} />\            <Route path="/profile" component={loggedIn ? Profile : Login} /> */}
+            <Route path="/profile" render={() => {
+              if(loggedIn) {
+                return <Profile />
+              } else {
+                return <Login />
+              }
+            }} exact />
+            <Route path="/home" component={() => <Home id={userToken}/>} exact />
+            <Redirect from="/" to="/home" />
             <Route path="/store/:name" component={StoreModal} />
           </IonRouterOutlet>
           <IonTabBar slot="bottom">
