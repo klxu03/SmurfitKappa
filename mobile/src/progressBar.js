bc8bd5f2f2e68a56fcf32c7f75f5b92e3d6829a1
mihirKachroo <60433268+mihirKachroo@users.noreply.github.com>
2020-04-25 22:23:24 -0400

    before bed

--- mobile/src/progressBar.js
+++ mobile/src/progressBar.js
@@ -5,7 +5,7 @@ import { IonProgressBar } from '@ionic/react';
 const Example: React.SFC<{}> = () => (
   <>
 
-    {/*-- Default Progressbar with 50 percent --*/}
+
     <IonProgressBar value={0.5}></IonProgressBar>
 
 
